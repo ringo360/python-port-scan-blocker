f973b1155988449e0928d56c70330d2c69893206
ringoXD <nep360i@gmail.com>
2024-03-14 22:07:00 +0900

    f

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import socket
 import subprocess
-# Create a raw socket with IPPROTO_IP protocol
+
+# IPPROTO_IPプロトコルを使用したrawソケットを作成します
 sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
 
-# Bind the socket to the network interface
-sock.bind(("your_interface_ip", 0))
+# ソケットをネットワークインターフェースにバインドします
+sock.bind(("0.0.0.0", 5640))
 
-# Set the socket to promiscuous mode
+# ソケットをプロミスキャスモードに設定します
 sock.ioctl(socket.SIO_SET_PMQ, 1)
 
 while True:
-    # Receive a packet
+    # パケットを受信します
     packet, addr = sock.recvfrom(1024)
 
-    # Check if the packet is an empty UDP packet
-    if packet[16:20] == b"\x00\x00\x00\x00" and addr[1] == 5640:
-        subprocess.run(["netsh", "advfirewall", "firewall", "add", "rule", "name=", "BlockEmptyUDP", "protocol=", "udp", "dir=", "in", "action=", "block", "remoteip=" + addr[0]])
-        print("Empty UDP packet detected from", addr[0])
-        
-
-
+    # パケットの宛先IPアドレスが192.168.1.200であるか確認します
+    if addr[0] == '192.168.1.200':
+        # パケットが空のUDPパケットかどうかを確認します
+        if packet[16:20] == b"\x00\x00\x00\x00" and addr[1] == 5640:
+            subprocess.run(["netsh", "advfirewall", "firewall", "add", "rule", "name=", "BlockEmptyUDP", "protocol=", "udp", "dir=", "in", "action=", "block", "remoteip=" + addr[0]])
+            print("空のUDPパケットが", addr[0], "から検出されました")
